9efcb696a214bb9c13b62a42bd9ae7a5d2a3016b
Anshu Goyal <72263458+Anshu-Goyal1@users.noreply.github.com>
2022-12-10 17:14:54 +0530

    Update File Handling..py

--- File Handling..py
+++ File Handling..py
@@ -1,16 +1,16 @@
- # writing a file 
+ # Writing a file 
 f=open('making a file.txt','w')
 f.write('name = kartikay class = 12  section = A' )
 f.close()
 
 
-#reading a text file with read mode
+#Reading a text file with read mode
 f=open('making a file.txt','r')
 print (f.read ())
 f.close()
 
 
-#reading a text file without reading mode (a+) appending mode
+#Reading a text file without reading mode (a+) appending mode
 f=open('making a file.txt','a+')
 print(f.read())
 f.write('\nschool name = don bosco sr sec school')
